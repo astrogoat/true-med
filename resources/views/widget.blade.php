24535a66f035ab2027abf825763f6fe85dca434a
Brett Michalick <michalick.brett@gmail.com>
2025-08-21 16:33:49 -0400

    fix for data-public-id

--- resources/views/widget.blade.php
+++ resources/views/widget.blade.php
@@ -1,3 +1,3 @@
 @if(app(Astrogoat\TrueMed\Settings\TrueMedSettings::class)->enabled)
-    <div id="truemed-instructions" icon-height="18" data-public-id="{{settings(Astrogoat\Postscript\Settings\PostscriptSettings::class, 'id')}}"></div>
+    <div id="truemed-instructions" icon-height="18" data-public-id="{{settings(Astrogoat\TrueMed\Settings\TrueMedSettings::class, 'id')}}"></div>
 @endif
